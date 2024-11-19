f80bab1fa4af77bf1e7f4689edaf7bc32bd3b83a
zhanggaoyu <zhanggy0630@tpl.cntaiping.com>
2024-11-19 16:11:49 +0800

    feat: add start file

---
+++ src/main/java/com/similarFish/task/flink/starter/FlinkDemo.java
@@ -0,0 +1,12 @@
+package com.similarFish.task.flink.starter;
+
+/**
+ * @Author zhanggaoyu
+ * @Date 2024/11/19
+ * @Description
+ */
+public class FlinkDemo {
+    public static void main(String[] args) {
+
+    }
+}
